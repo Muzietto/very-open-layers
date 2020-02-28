570927eeb90615a1ff4db4289cf8ce55a086970c
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2020-02-28 15:07:55 +0100

    example 02 drawing the ugly OSM popup marker + failed simple ICON stuff

--- 02_OSM_embedded_example/js/functions.js
+++ 02_OSM_embedded_example/js/functions.js
@@ -32,43 +32,64 @@ function addMarker(layer, lon, lat, popupContentHTML) {
 	marker = new OpenLayers.Marker(ll);
 	marker.feature = feature;
 
-				var markerClick = function(evt) {
-					if (this.popup == null) {
-						this.popup = this.createPopup(this.closeBox);
-						map.addPopup(this.popup);
-						this.popup.show();
-					} else {
-						this.popup.toggle();
-					}
-					OpenLayers.Event.stop(evt);
-				};
-				marker.events.register('mousedown', feature, markerClick);
+	var markerClick = function(evt) {
+		if (this.popup == null) {
+			this.popup = this.createPopup(this.closeBox);
+			map.addPopup(this.popup);
+			this.popup.show();
+		} else {
+			this.popup.toggle();
+		}
+		OpenLayers.Event.stop(evt);
+	};
 
-				layer.addMarker(marker);
-				map.addPopup(feature.createPopup(feature.closeBox));
-			}
+	marker.events.register('mousedown', feature, markerClick);
 
-			function getTileURL(bounds) {
-				var res = this.map.getResolution();
-				var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
-				var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
-				var z = this.map.getZoom();
-				var limit = Math.pow(2, z);
-				if (y < 0 || y >= limit) {
-					return null;
-				} else {
-					x = ((x % limit) + limit) % limit;
-					url = this.url;
-					path= z + '/' + x + '/' + y + '.' + this.type;
-					if (url instanceof Array) {
-						url = this.selectUrl(path, url);
-					}
-					return url+path;
-				}
-			}
+	layer.addMarker(marker);
+	map.addPopup(feature.createPopup(feature.closeBox));
+}
 
-			// Map event listener moved
-			function mapEventMove(event) {
-				// Update harbour layer
-				refreshHarbours();
-			}
+// not working!!
+function marcoAddMarker(layer, lon, lat, color) {
+  var iconUrl = {
+    red: './js/img/marker.png',
+    blue: './js/img/marker-blue.png',
+    gold: './js/img/marker-gold.png',
+    green: './js/img/marker-green.png',
+  }[color] || './js/img/marker.png';
+
+  var size = new OpenLayers.Size(21,25);
+  var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+  var icon = new OpenLayers.Icon(iconUrl);
+
+  try {
+    layer.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(lon, lat), icon));
+  } catch (err) {
+    console.log('provided layer is not an OpenLayers.Layer.Markers');
+  }
+}
+
+function getTileURL(bounds) {
+	var res = this.map.getResolution();
+	var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+	var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+	var z = this.map.getZoom();
+	var limit = Math.pow(2, z);
+	if (y < 0 || y >= limit) {
+		return null;
+	} else {
+		x = ((x % limit) + limit) % limit;
+		url = this.url;
+		path= z + '/' + x + '/' + y + '.' + this.type;
+		if (url instanceof Array) {
+			url = this.selectUrl(path, url);
+		}
+		return url+path;
+	}
+}
+
+// Map event listener moved
+function mapEventMove(event) {
+	// Update harbour layer
+	refreshHarbours();
+}
--- 02_OSM_embedded_example/js/init.js
+++ 02_OSM_embedded_example/js/init.js
@@ -6,9 +6,9 @@ var layer_seamark;
 var marker;
 
 // Position and zoomlevel of the map
-var lon = 12.0915;
-var lat = 54.1878;
-var zoom = 15;
+var lon = 9.114508;
+var lat = 39.211631;
+var zoom = 12;
 
 var linkTextSkipperGuide = 'Beschreibung auf SkipperGuide';
 var linkTextWeatherHarbour = 'Meteogramm';
@@ -24,20 +24,31 @@ function drawmap() {
 			//'zoomend': mapEventZoom
 		},
 		controls: [
-			new OpenLayers.Control.Navigation(),
-			new OpenLayers.Control.ScaleLine({
+      // control mouse events
+			new OpenLayers.Control.Navigation(), // http://dev.openlayers.org/docs/files/OpenLayers/Control/Navigation-js.html
+      // draw black horiz line with notches bottom left of page
+			new OpenLayers.Control.ScaleLine({ // http://dev.openlayers.org/docs/files/OpenLayers/Control/ScaleLine-js.html
         topOutUnits : 'nmi',
         bottomOutUnits: 'km',
         topInUnits: 'nmi',
         bottomInUnits: 'km',
         maxWidth: '40'
       }),
+      // allows the user interface to switch between BaseLayers and to show or hide Overlays.
 			new OpenLayers.Control.LayerSwitcher(),
-			new OpenLayers.Control.MousePosition(),
+      // mouse coordinates lat/lon bottom right of the map
+			new OpenLayers.Control.MousePosition({
+        prefix: '<a target="_blank" ' +
+            'href="http://spatialreference.org/ref/epsg/4326/">' +
+            'EPSG:4326</a> coordinates: '
+      }),
+      // By default it is displayed in the upper left corner of the map as 4 directional arrows above a vertical slider.
 			new OpenLayers.Control.PanZoomBar()
     ],
+    // Instances of this class represent bounding boxes.  Data stored as left, bottom, right, top floats.
 		maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34),
 		numZoomLevels: 18,
+    // Required if you are not displaying the whole world on a tile with the size specified in tileSize.
 		maxResolution: 156543,
 		units: 'meters',
 	});
@@ -66,13 +77,28 @@ function drawmap() {
     });
 	layer_pois.setOpacity(0.8);
 
+  var markers = new OpenLayers.Layer.Markers("Markers");
+
 	map.addLayers([
     layer_mapnik,
     layer_seamark,
-    layer_pois
+    layer_pois,
+    markers,
   ]);
+
 	jumpTo(lon, lat, zoom);
 
-	// Update harbour layer
+  addMarker(markers, 9.20961, 39.19487, '<p>CIAO CIAO</p>');
+  //marcoAddMarker(markers, 9.07262, 39.16227, 'gold');
+
+  // var markers = new OpenLayers.Layer.Markers( "Markers" );
+  // map.addLayer(markers);
+  //
+  // var size = new OpenLayers.Size(21,25);
+  // var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+  // var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+  // markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon));
+  // markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon.clone()));
+  // Update harbour layer
 	refreshHarbours();
 }
